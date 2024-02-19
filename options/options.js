4f347e585e993afc418602723a4e91dd7852e132
Crowdbotics <team@crowdbotics.com>
2024-02-19 13:00:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-math-46462",
+  url: "https://fancy-math-46462.botics.co",
+  api: "https://fancy-math-46462.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
